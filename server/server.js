bbffe4dc149f6c32f0cfe746f0d6c2aa117243bc
iamharbie <mi_haby@live.com>
2018-04-29 19:32:19 +0100

    Set up for heroku build done

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 const publicPath = path.join(__dirname, '..', 'public');
 
 
@@ -12,6 +14,6 @@ app.get('*', (req,res)=>{
 });
 
 
-app.listen(3000, ()=> {
+app.listen(port, ()=> {
     console.log('App is live');
 });
